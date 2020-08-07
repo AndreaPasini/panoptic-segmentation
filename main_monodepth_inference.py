d673fc38aaf716c5d0e84b3d31bd28d9ea6717b1
andrea <andrea.pasini@polito.it>
2020-08-07 17:20:48 +0200

    Monodepth Inference.

--- main_monodepth_inference.py
+++ main_monodepth_inference.py
@@ -1,13 +1,20 @@
+from PIL import Image
+
 from config import out_depth_train_dir, COCO_img_train_dir, out_depth_val_dir, COCO_img_val_dir
 from monodepth.monodepth import Monodepth
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import repeat
 import cv2
 from os import getpid
+import os
+import time
+from multiprocessing import Pool
 
+def run_model(img_ids, input_dir, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-
-def run_model(img_ids):
     pid = getpid()
 
     step = len(img_ids)//100
@@ -20,10 +27,12 @@ def run_model(img_ids):
     print(f"P{pid} - Start to compute detections...")
     for i, img_id in enumerate(img_ids):
         # Read image and predict
-        img = cv2.imread(img_id)
+        img = cv2.imread(os.path.join(input_dir, img_id+'.jpg'))
         pred = monodepth.predict(img)
         # Save detection
-        out_depth_train_dir
+        outimg = Image.fromarray((pred*255).astype(np.uint8), mode='L')
+        outimg.save(os.path.join(output_dir, img_id+'.png'))
+        #cv2.imwrite(os.path.join(output_dir, img_id+'b.png'), img)
 
         if i%step == 0:
             print(f"P{pid} - {100.0*(i+1)/len(img_ids)}%, {i+1} images.")
@@ -33,6 +42,7 @@ def run_model(img_ids):
 def main():
     ### Choose methods to be run ###
     class RUN_CONFIG:
+        num_proc = 50
         compute_train2017_depth = True
         compute_val2017_depth = False
 
@@ -43,24 +53,24 @@ def main():
         output_dir = out_depth_val_dir
         input_dir = COCO_img_val_dir
 
-    monodepth = Monodepth()
-    image_path = "../COCO/images/train2017/000000000074.jpg"
-    img = cv2.imread(image_path)
-    res = monodepth.predict(img)
 
-    # Saving colormapped depth image
-    vmax = np.percentile(res, 95)
+    # Files to be analyzed
+    input_files = {f.split('.')[0] for f in os.listdir(input_dir)}
+    output_files = {f.split('.')[0] for f in os.listdir(output_dir)}
+    files = sorted(list(input_files-output_files))
 
-    plt.figure(figsize=(10, 10))
-    plt.subplot(211)
-    plt.imshow(img)
-    plt.title("Input", fontsize=22)
-    plt.axis('off')
+    pool = Pool(RUN_CONFIG.num_proc)
+    start = time.time()
 
-    plt.subplot(212)
-    plt.imshow(res, cmap='magma', vmax=vmax)
-    plt.title("Disparity prediction", fontsize=22)
-    plt.axis('off');
+    # Apply to images
+    chuncks = np.array_split(np.array(files), RUN_CONFIG.num_proc)
+    pool.starmap(run_model, zip(chuncks, repeat(input_dir), repeat(output_dir)))
+
+    end = time.time()
+    print(end - start)
+    pool.close()
+    pool.join()
+
 
 if __name__=='__main__':
     main()
